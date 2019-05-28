2d2e2391170a5a9ed34d5a232b65c3412cfb4960
仲前彰真 <andy@ubi.cs.ritsumei.ac.jp>
2019-05-28 10:51:53 +0900

    wifiAware実装完了 publishボタンを押すとメッセージ受信待ち subscribeボタンを押すと"hello"メッセージ送信 通信を行うとpeerHandleが登録され、wifi rttに利用可能

--- app/src/main/java/com/example/wi_fi_rtt_mesure/MainActivity.java
+++ app/src/main/java/com/example/wi_fi_rtt_mesure/MainActivity.java
@@ -10,6 +10,10 @@ import android.net.wifi.rtt.WifiRttManager;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,11 +25,48 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        TextView aware_check = findViewById(R.id.aware_check);
+        TextView rtt_check = findViewById(R.id.rtt_check);
+        TextView aware_avail = findViewById(R.id.aware_avail);
+        TextView rtt_avail = findViewById(R.id.rtt_avail);
+        Button check_button = findViewById(R.id.check_permission);
+        Button pub_button = findViewById(R.id.publish);
+        Button sub_button = findViewById(R.id.subscribe);
+        Button rtt_button = findViewById(R.id.wifi_rtt);
+
         context = getApplicationContext();
 
-        WifiManager wifiManager = new WifiManager(context, handler);
+        final WifiManager wifiManager = new WifiManager(context, handler);
 
-        wifiManager.publish();
+        wifiManager.check(aware_check, rtt_check);
+        wifiManager.available(aware_avail, rtt_avail);
 
+        check_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                wifiManager.check_permission();
+            }
+        });
+
+        pub_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                wifiManager.publisher();
+            }
+        });
+
+        sub_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                wifiManager.subscriber();
+            }
+        });
+
+        rtt_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                wifiManager.connectRtt();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/wi_fi_rtt_mesure/WifiManager.java
+++ app/src/main/java/com/example/wi_fi_rtt_mesure/WifiManager.java
@@ -23,6 +23,7 @@ import android.net.wifi.rtt.WifiRttManager;
 import android.os.Handler;
 import android.support.v4.app.ActivityCompat;
 import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.List;
@@ -33,11 +34,11 @@ public class WifiManager {
     public Context context;
     private final WifiAwareManager wifiAwareManager;
     private final WifiRttManager wifiRttManager;
-    private final IntentFilter filter;
     private WifiAwareSession awareSession;
     private Handler mHandler;
     private final Executor executor;
-    public static final String AWARE_SERVICE_NAME = "Aware Service";
+    public static final String AWARE_SERVICE_NAME = "Aware";
+    private SubscribeDiscoverySession session;
 
     public PeerHandle peerHandle = null;
 
@@ -45,78 +46,68 @@ public class WifiManager {
         this.context = context;
         wifiAwareManager = (WifiAwareManager) context.getSystemService(Context.WIFI_AWARE_SERVICE);
         wifiRttManager = (WifiRttManager) context.getSystemService(Context.WIFI_RTT_RANGING_SERVICE);
-        filter = new IntentFilter(WifiAwareManager.ACTION_WIFI_AWARE_STATE_CHANGED);
         mHandler = handler;
         executor = context.getMainExecutor();
 
-        checkAvailable();
+        wifiAwareManager.attach(new myAttachCallback(), mHandler);
     }
 
-    public void publish() {
-        wifiAwareManager.attach(new pubAttachCallback(), mHandler);
-    }
-
-    public void subscribe(){
-        wifiAwareManager.attach(new subAttachCallback(), mHandler);
-    }
-
-    class pubAttachCallback extends AttachCallback {
+    class myAttachCallback extends AttachCallback {
 
         public void onAttachFailed() {
+            super.onAttachFailed();
             Toast.makeText(context, "onAttachFailed", Toast.LENGTH_SHORT).show();
         }
 
         public void onAttached(WifiAwareSession session) {
+            super.onAttached(session);
             Toast.makeText(context, "onAttach", Toast.LENGTH_SHORT).show();
-
             awareSession = session;
-            PublishConfig config = new PublishConfig.Builder()
-                    .setServiceName(AWARE_SERVICE_NAME)
-                    .build();
+        }
+    }
+
+    public void publisher(){
+        PublishConfig config = new PublishConfig.Builder()
+                .setServiceName(AWARE_SERVICE_NAME)
+                .build();
+
+        if(awareSession != null) {
             awareSession.publish(config, new DiscoverySessionCallback() {
                 @Override
                 public void onPublishStarted(PublishDiscoverySession session) {
-                    Toast.makeText(context, "onPublishStarted", Toast.LENGTH_SHORT).show();
+                    super.onPublishStarted(session);
+                    Toast.makeText(context, "Service published! Waiting for Subscriber", Toast.LENGTH_SHORT).show();
                 }
-
                 @Override
                 public void onMessageReceived(PeerHandle peerHandle, byte[] message) {
+                    super.onMessageReceived(peerHandle, message);
+                    Toast.makeText(context, "Message Received" + message, Toast.LENGTH_SHORT).show();
                     WifiManager.this.peerHandle = peerHandle;
-                    Toast.makeText(context, "onMessageReceived : " + message.toString(), Toast.LENGTH_SHORT).show();
                 }
             }, null);
         }
     }
 
-    class subAttachCallback extends AttachCallback {
-        @Override
-        public void onAttachFailed() {
-            Toast.makeText(context, "onAttachFailed", Toast.LENGTH_SHORT).show();
-        }
+    public void subscriber(){
+        SubscribeConfig config = new SubscribeConfig.Builder()
+                .setServiceName(AWARE_SERVICE_NAME)
+                .build();
 
-        @Override
-        public void onAttached(WifiAwareSession session) {
-            Toast.makeText(context, "onAttach", Toast.LENGTH_SHORT).show();
-
-            awareSession = session;
-
-            SubscribeConfig config = new SubscribeConfig.Builder()
-                    .setServiceName(AWARE_SERVICE_NAME)
-                    .build();
-
+        if(awareSession != null) {
             awareSession.subscribe(config, new DiscoverySessionCallback() {
-
                 @Override
                 public void onSubscribeStarted(SubscribeDiscoverySession session) {
-                    if (peerHandle != null) {
-                        int messageId = 1;
-                        session.sendMessage(peerHandle, messageId, "Test Message".getBytes());
-                    }
+                    super.onSubscribeStarted(session);
+                    WifiManager.this.session = session;
+                    Toast.makeText(context, "Subscriber started", Toast.LENGTH_SHORT).show();
                 }
 
                 @Override
-                public void onServiceDiscovered(PeerHandle peerHandle,
-                                                byte[] serviceSpecificInfo, List<byte[]> matchFilter) {
+                public void onServiceDiscovered(PeerHandle peerHandle, byte[] serviceSpecificInfo, List<byte[]> matchFilter) {
+                    super.onServiceDiscovered(peerHandle, serviceSpecificInfo, matchFilter);
+                    Toast.makeText(context, "Service discoverd" + peerHandle.toString() + "\t sending message now", Toast.LENGTH_SHORT).show();
+                    String msg = "hello";
+                    session.sendMessage(peerHandle, 1, msg.getBytes());
                     WifiManager.this.peerHandle = peerHandle;
                 }
             }, null);
@@ -124,36 +115,58 @@ public class WifiManager {
         }
     }
 
-    private void checkAvailable() {
+    public void check(TextView aware, TextView rtt) {
         if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_WIFI_AWARE)) {
-            System.out.println("Android Wifi supports Wi-Fi Aware : True");
+            aware.setText("True");
         } else {
-            System.out.println("Android Wifi supports Wi-Fi Aware : False");
+            aware.setText("False");
         }
-
         if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_WIFI_RTT)) {
-            System.out.println("Android Wifi supports RTT : True");
+            rtt.setText("True");
         } else {
-            System.out.println("Android Wifi supports RTT : False");
+            rtt.setText("False");
+        }
+    }
+
+
+    public void available(final TextView aware, final TextView rtt){
+        if (wifiAwareManager.isAvailable()) {
+            aware.setText("True");
+        } else {
+            aware.setText("False");
         }
 
-        BroadcastReceiver myReceiver = new BroadcastReceiver() {
+        if (wifiRttManager.isAvailable()) {
+            rtt.setText("True");
+        } else {
+            rtt.setText("False");
+        }
+
+        IntentFilter aware_filter = new IntentFilter(WifiAwareManager.ACTION_WIFI_AWARE_STATE_CHANGED);
+        BroadcastReceiver aware_Receiver = new BroadcastReceiver() {
             @Override
             public void onReceive(Context context, Intent intent) {
                 if (wifiAwareManager.isAvailable()) {
-                    System.out.println("Wi-Fi Aware is available : True");
+                    aware.setText("True");
                 } else {
-                    System.out.println("Wi-Fi Aware is available : False");
-                }
-
-                if (wifiRttManager.isAvailable()) {
-                    System.out.println("Wi-Fi RTT is available : True");
-                } else {
-                    System.out.println("Wi-Fi RTT is available : False");
+                    aware.setText("False");
                 }
             }
         };
-        context.registerReceiver(myReceiver, filter);
+        context.registerReceiver(aware_Receiver, aware_filter);
+
+        IntentFilter rtt_filter = new IntentFilter(WifiRttManager.ACTION_WIFI_RTT_STATE_CHANGED);
+        BroadcastReceiver rtt_Receiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                if (wifiRttManager.isAvailable()) {
+                    rtt.setText("True");
+                } else {
+                    rtt.setText("False");
+                }
+            }
+        };
+        context.registerReceiver(rtt_Receiver, rtt_filter);
     }
 
     public void connectRtt() {
@@ -182,5 +195,15 @@ public class WifiManager {
 
             }
         });
+    }
+
+    public void check_permission(){
+        if (ActivityCompat.checkSelfPermission(
+                context, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            Log.d("check", "ACCESS_FINE_LOCATION permission OK");
+        } else {
+            Log.d("check", "ACCESS_FINE_LOCATION permission NG");
+            return;
+        }
     }
 }
